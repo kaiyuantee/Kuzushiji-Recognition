3c8fca5a0c22c08d50134cad9fecc4f97b403712
wintersin <kaiyuan.tee1@gmail.com>
2020-01-20 03:04:49 +0800

    added shell script without classification for testing

---
+++ runner.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for fold in {0..4};
+do
+	echo "this is fold ${fold}"
+	#train the frcnn model
+	python -m kuzushiji.object_detection.main --output-dir _runs/detection_fold${fold} \
+	--fold ${fold} --model fasterrcnn_resnet50_fpn
+	#oof predictions
+	python -m kuzushiji.object_detection.main --output-dir _runs/detection_fold${fold} \
+        --fold ${fold} --model fasterrcnn_resnet50_fpn --resume _runs/detection_fold${fold}/model_best.p \
+        --test-only
+	#inference
+	python -m kuzushiji.object_detection.main --output-dir _runs/detection_fold${fold} \
+        --fold ${fold} --model fasterrcnn_resnet50_fpn --resume _runs/detection_fold${fold}/model_best.p \
+        --submission
+done
+
+#blend the predictions from all 5 folds
+python -c "
+import pandas as pd;
+pd.concat([pd.read_csv(f'_runs/segment-fold{i}/clf_gt.csv') for i in range(5)]).to_csv('_runs/segment_clf_gt.csv')
+"
